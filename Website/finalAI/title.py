7e1160a4d2125b81a362a511f5181308685d5f48
ysyloveu <41413469+ysyloveu@users.noreply.github.com>
2020-06-14 22:07:37 +0800

    Update title.py

--- Website/finalAI/title.py
+++ Website/finalAI/title.py
@@ -1,18 +1,33 @@
+
+
+from bs4 import BeautifulSoup
+import urllib.request
+import gzip
+import io
 import sys
-
 def main(url):
-    import requests
-    from bs4 import BeautifulSoup
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0'}
 
-    r1 = requests.get(url)
-    r1.status_code
 
-    coverpage = r1.content
-    soup = BeautifulSoup(coverpage, 'html5lib')
-    title = soup.find_all('title') 
+    req = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(req)
 
-    for x in title:
+    if response.info().get('Content-Encoding') == 'gzip':
+        pagedata = gzip.decompress(response.read())
+    elif response.info().get('Content-Encoding') == 'deflate':
+        pagedata = response.read()
+    elif response.info().get('Content-Encoding'):
+        print('Encoding type unknown')
+    else:
+        pagedata = response.read()
+
+    soup = BeautifulSoup(pagedata,'html5lib')
+    contents = soup.find_all('title')   
+    print(len(contents))
+    for x in contents:
         print(x.get_text())
-
 if __name__=='__main__':
     main(sys.argv[1])
