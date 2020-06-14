321f673e4a345b248ed4d8bd0484dd0e5c937562
ysyloveu <41413469+ysyloveu@users.noreply.github.com>
2020-06-14 22:05:52 +0800

    Update scraperfunc.py
    change due to straits time change their header

--- Website/scraperfunc.py
+++ Website/scraperfunc.py
@@ -1,25 +1,34 @@
-#!C:\Users\Alfred\AppData\Local\Programs\Python\Python37\python.exe
+
+
+from bs4 import BeautifulSoup
+import urllib.request
+import gzip
+import io
 import sys
-
 def main(url):
-    import requests
-    from bs4 import BeautifulSoup
+    #url = 'https://www.straitstimes.com/asia/australianz/new-zealand-city-takes-down-statue-of-british-navy-commander?cx_testId=20&cx_testVariant=cx_1&cx_artPos=0#cxrecs_s'
+
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate',
+               'Accept-Language': 'en-US,en;q=0.5',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0'}
 
 
-# Request
-    r1 = requests.get(url)
-    r1.status_code
+    req = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(req)
 
-# We'll save in coverpage the cover page content
-    coverpage = r1.content
+    if response.info().get('Content-Encoding') == 'gzip':
+        pagedata = gzip.decompress(response.read())
+    elif response.info().get('Content-Encoding') == 'deflate':
+        pagedata = response.read()
+    elif response.info().get('Content-Encoding'):
+        print('Encoding type unknown')
+    else:
+        pagedata = response.read()
 
-# Soup creation
-    soup = BeautifulSoup(coverpage, 'html5lib')
-
-# News identification
+    soup = BeautifulSoup(pagedata,'html5lib')
     contents = soup.find_all('p')   
     print(len(contents))
-#print(contents)
     for x in contents:
         print(x.get_text())
 if __name__=='__main__':
